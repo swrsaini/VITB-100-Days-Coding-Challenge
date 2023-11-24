bfcc4d0832717ecd0957973d27ed84145f0c911a
swrsaini <151948496+swrsaini@users.noreply.github.com>
2023-11-24 23:33:36 +0530

    Completed

---
+++ day 12/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+class Hanoi {
+
+    public long toh(int N, int from, int to, int aux) {
+        
+        long moves = 0;
+        if(N == 1){
+            System.out.println("move disk 1 from rod " + from + " to rod " + to);
+            return 1;
+        }
+        
+        moves += toh (N-1, from, aux, to);
+        System.out.println("move disk " + N + " from rod " + from + " to rod " + to);
+        moves++;
+        
+        moves += toh(N-1, aux, to, from);
+        return moves;
+    }
+}
